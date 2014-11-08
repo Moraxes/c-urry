82a0347ff6dd66cf55292ae3bf9d248cafb53fe1
Maciej Śniegocki <madgod112@gmail.com>
2014-11-08 15:36:21 +0100

    Updated example and put it in file. Also updated readme.

---
+++ example.cpp
@@ -0,0 +1,21 @@
+// To build: g++ -std=c++14 example.cpp -o example
+
+#include <iostream>
+#include "curry.hpp"
+
+int add_then_sub(int x, int y, int z) {
+    return x + y - z;
+}
+
+// auto return type to save lots of typing
+// this is also an example of eta reduction
+auto step_one() {
+    // first template parameter is the return type, the others are arguments' types
+    return curry(add_then_sub)(5);
+}
+
+int main(int argc, char* argv[]) {
+    std::cout << step_one()(4)(3) << "\n";
+
+    return 0;
+}
